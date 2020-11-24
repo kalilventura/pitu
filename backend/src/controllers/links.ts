b0a125c549ebdb802c2e11f2bc5477156548fa60
Kalil Teixeira Ventura Monteiro <kalilventur@hotmail.com>
2020-11-23 21:27:01 -0300

    Backend

---
+++ backend/src/controllers/links.ts
@@ -0,0 +1,52 @@
+import { Link } from '../models/link';
+import { Request, Response } from 'express';
+import linksRepository from '../models/linksRepository';
+
+function generateCode() {
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (let i = 0; i < 5; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+
+    return text;
+}
+
+async function postLink(req: Request, res: Response) {
+    const link = req.body as Link;
+    link.code = generateCode();
+    link.hits = 0;
+
+    const result = await linksRepository.add(link);
+    if (!result.id) return res.sendStatus(400);
+
+    link.id = result.id!;
+    res.status(201).json(link);
+}
+
+async function getLink(req: Request, res: Response) {
+    const code = req.params.code as string;
+    const link = await linksRepository.findByCode(code);
+    if (!link)
+        res.sendStatus(404);
+    else
+        res.status(200).json(link);
+}
+
+function hitLink(req: Request, res: Response) {
+    const code = req.params.code as string;
+    const link = linksRepository.hit(code);
+
+    if (!link)
+        return res.sendStatus(404);
+    else {
+        res.json(link);
+    }
+}
+
+export default {
+    postLink,
+    getLink,
+    hitLink
+}
