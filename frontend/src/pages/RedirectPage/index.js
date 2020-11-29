2e6806dbfc999c8e3a4ff0776e0ff6b0c89bbd88
Kalil Teixeira Ventura Monteiro <kalilventur@hotmail.com>
2020-11-29 19:46:50 -0300

    Correcao da redirect page

--- frontend/src/pages/RedirectPage/index.js
+++ frontend/src/pages/RedirectPage/index.js
@@ -1,55 +1,87 @@
 import React from 'react';
 import Header from '../../components/Header';
 import { Container } from 'react-bootstrap';
+import ShortenerService from '../../services/shortenerService';
+import { parseISO, formatRelative } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import ShortenerSevice from '../../services/shortenerService';
-import { StatsContainer } from './styles';
+import { StatsContainer, StatsBoxTitle, StatsRow, StatsBox } from './styles';
+import vars from '../../configs/vars';
 
-export default class RedirectPage extends React.Component {
+export default class StatsPage extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
             isLoading: false,
-            url: '',
-            errorMessage: '',
-        }
+            shortenedURL: {},
+            errorMessage: ""
+        };
+
     }
 
     async componentDidMount() {
         const { code } = this.props.match.params;
-
         try {
-            const service = new ShortenerSevice();
-            const { url } = await service.getLink(code);
+            const service = new ShortenerService();
+            const shortenedURL = await service.getStats(code);
 
-            window.location = url;
+            const parsedDate = parseISO(shortenedURL.updatedAt);
+            const currentDate = new Date();
+            const relativeDate = formatRelative(parsedDate, currentDate, {
+                locale: ptBR
+            });
 
+            shortenedURL.relativeDate = relativeDate;
+
+            this.setState({
+                isLoading: false,
+                shortenedURL
+            });
         } catch (error) {
-            this.setState({ isLoading: false, erorMessage: 'Ops, a url solicitada não existe.' });
+            this.setState({
+                isLoading: false,
+                errorMessage: `Ops... a URL informada não existe !`
+            });
         }
     }
 
     render() {
-        const { errorMessage } = this.state;
+        const { errorMessage, shortenedURL } = this.state;
         return (
             <Container>
-                {errorMessage ? (
-                    <>
-                        <Header>
-                            Seu novo Encurtador de urls. :)
-                        </Header>
-                        <StatsContainer className="text-center">
-                            <FontAwesomeIcon size="3x" color="#f8d7da" icon="faExclamationTriangle" />
-                            <p className="m-3">{errorMessage}</p>
-                            <a className="btn btn-primary" href="/">Encurtar nova URL</a>
-                        </StatsContainer>
-                    </>
-                ) : (
-                        <p className="text-center">Redirecionando</p>
-                    )
-                }
+                <Header>Estatísticas de URL encurtada</Header>
+                <StatsContainer className="text-center">
+                    {
+                        errorMessage ? (
+                            <>
+                                <FontAwesomeIcon size="3x" color="#f8d7da" icon="exclamation-triangle" />
+                                <p className="mt-2 mp-2">{errorMessage}</p>
+                                <a href="/" className="btn btn-primary">Encurtar outra URL</a>
+                            </>
+                        ) : (
+                                <>
+                                    <p>Confira abaixo as estatísticas de acesso de sua URL.</p><br />
+                                    <h4>{vars.HOST_APP + shortenedURL.code}</h4>
+                                    <p>Redirecionando para : <b>{shortenedURL.url}</b></p>
+                                    <StatsRow>
+                                        <StatsBox className="col-12 col-lg-6">
+                                            <b>{shortenedURL.hits}</b>
+                                            <StatsBoxTitle>visitas</StatsBoxTitle>
+                                        </StatsBox>
+                                        <StatsBox className="col-12 col-lg-6">
+                                            <b>{shortenedURL.relativeDate}</b>
+                                            <StatsBoxTitle>último acesso</StatsBoxTitle>
+                                        </StatsBox>
+                                    </StatsRow>
+                                    <hr />
+                                    <a href="/" className="btn btn-primary">Encurtar outra URL</a>
+                                    <br /><br />
+                                </>
+                            )
+                    }
+                </StatsContainer>
             </Container>
-        )
+        );
     }
 }
--- frontend/src/pages/RedirectPage/styles.js
+++ frontend/src/pages/RedirectPage/styles.js
@@ -1,5 +1,30 @@
 import styled from 'styled-components';
 
 export const StatsContainer = styled.div`
-display: block;
+    display:block;
+`;
+
+export const StatsRow = styled.div`
+    display:flex;
+    flex-wrap:wrap;
+    justify-content: center !important;
+    margin-bottom:0.5rem;
+`;
+
+export const StatsBox = styled.div`
+    padding:2rem;
+    border:1px solid #ccc;
+    border-radius : .25rem;
+    text-aling:center;
+    margin:.5rem;
+    display:block;
+    float:left;
+    border-top:4px solid #ccc;
+    box-shadow:0 0 10px #ccc;
+`;
+
+export const StatsBoxTitle = styled.div`
+    font-weight:500;
+    padding:0.25.rem;
+    display:block;
 `;
