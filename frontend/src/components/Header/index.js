7fbdf1e1c4a140bdeb2eb3ac3d64a192f37eb683
Kalil Teixeira Ventura Monteiro <kalilventur@hotmail.com>
2020-11-29 15:14:02 -0300

    Colocando um icone

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { HeaderContainer, Logo } from './styles';
-import { faLink } from '@fortawesome/free-solid-svg-icons';
+import Icone from "../../assets/icone.png";
 
 const Header = (props) => {
     return (
         <>
             <HeaderContainer>
-                <Logo src={faLink} alt="Pitu - Encurtador de URL" />
+                <Logo src={Icone} alt="Pitu - Encurtador de URL" />
                 <h1>Pitu</h1>
                 <p>{props.children}</p>
             </HeaderContainer>
